fcd9195be023a805ee143f8f899967fb47fe2d7e
Thomas Frössman <thomasf@jossystem.se>
2016-09-09 19:22:02 +0200

    Refactor

--- __init__.py
+++ __init__.py
@@ -1,15 +1,40 @@
-#Embedded file name: /Users/versonator/Hudson/live/Projects/AppLive/Resources/MIDI Remote Scripts/LPD8/__init__.py
-from _Generic.GenericScript import GenericScript
+# Embedded file name:
+# /Users/versonator/Hudson/live/Projects/AppLive/Resources/MIDI Remote
+# Scripts/LPD8/__init__.py
+
 import Live
-from config import *
+import _Framework.Capabilities as caps
+from _Generic.GenericScript import GenericScript
+from twisterhk import config
+
 
 def create_instance(c_instance):
     """ The generic script can be customised by using parameters (see config.py). """
-    return GenericScript(c_instance, Live.MidiMap.MapMode.absolute, Live.MidiMap.MapMode.absolute, DEVICE_CONTROLS, TRANSPORT_CONTROLS, VOLUME_CONTROLS, TRACKARM_CONTROLS, BANK_CONTROLS, CONTROLLER_DESCRIPTION)
+    return GenericScript(
+        c_instance,
+        Live.MidiMap.MapMode.absolute,
+        Live.MidiMap.MapMode.absolute,
+        config.DEVICE_CONTROLS,
+        config.TRANSPORT_CONTROLS,
+        config.VOLUME_CONTROLS,
+        config.TRACKARM_CONTROLS,
+        config.BANK_CONTROLS,
+        config.CONTROLLER_DESCRIPTION,
+    )
 
-
-from _Framework.Capabilities import *
 
 def get_capabilities():
-    return {CONTROLLER_ID_KEY: controller_id(vendor_id=2536, product_ids=[117], model_name='LPD8'),
-     PORTS_KEY: [inport(props=[NOTES_CC, REMOTE, SCRIPT]), outport(props=[SCRIPT])]}
+    return {
+        caps.CONTROLLER_ID_KEY: caps.controller_id(
+            vendor_id=2536,
+            product_ids=[117],
+            model_name='LPD8'),
+        caps.PORTS_KEY: [
+            caps.inport(props=[
+                caps.NOTES_CC,
+                caps.REMOTE,
+                caps.SCRIPT,
+            ]),
+            caps.outport(props=[caps.SCRIPT]),
+        ],
+    }
